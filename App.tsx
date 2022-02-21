89ba26342deb49e47a1f5c36d6564f76faf3239b
dalila <dalila.yazit@gmail.com>
2022-02-21 16:12:19 +0100

    ajout du fichier termsscreen et du scrool

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
+import { TermsScreen } from './src/screens/TermsScreen';
+//import { WhatEverScreen } from './src/screens/WhatEverScreen';
+
+
 
 const App = () => {
-  return <LoginScreen />;
+  // return <LoginScreen />; 
+  return <TermsScreen />;
+  //return <WhatEverScreen />;
 };
 
 export default App;
