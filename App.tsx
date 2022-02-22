2e78553d60054a538bdd2c3ebc104395abc6c3cd
dalila <dalila.yazit@gmail.com>
2022-02-22 09:02:47 +0100

    ajout du fichier StarshipFeedScreen et du data.json

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
+import { StarshipFeedScreen } from './src/screens/StarshipFeedScreen';
 import { TermsScreen } from './src/screens/TermsScreen';
-//import { WhatEverScreen } from './src/screens/WhatEverScreen';
+//import { WhatEverScreen } from "./src/screens/WhatEverScreen";
 
 
 
 const App = () => {
   // return <LoginScreen />; 
-  return <TermsScreen />;
-  //return <WhatEverScreen />;
+  // return <TermsScreen />;
+  // return <WhatEverScreen />
+  return <StarshipFeedScreen />;
 };
 
 export default App;
